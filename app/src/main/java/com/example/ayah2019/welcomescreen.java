a2d9d3b310ba3a86340c70b110ad746ab4a7e830
Owner <lama76618@gmail.com>
2019-12-09 08:44:39 +0200

    PHOTO

---
+++ app/src/main/java/com/example/ayah2019/welcomescreen.java
@@ -0,0 +1,51 @@
+package com.example.ayah2019;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class welcomescreen extends AppCompatActivity {
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Thread th=new Thread()
+        {
+            @Override
+            public void run() {
+                try {
+                    sleep(3*1000);
+                    FirebaseAuth auth=FirebaseAuth.getInstance();
+                    if(auth.getCurrentUser()==null||auth.getCurrentUser().getEmail()==null){
+                        Intent i=new Intent(getApplication(),signinActivity.class);
+                        startActivity(i);
+                        finish();
+                    }
+                    else
+                    {
+                        Intent i=new Intent(getApplication(),searchMidicine.class);
+                        startActivity(i);
+                        finish();
+
+                    }
+
+
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+
+            }
+        };
+
+        th.start();
+    }
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcomescreen);
+    }
+}
