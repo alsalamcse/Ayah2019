9e6cef170526fc2c2aab1543538e64a90d9cef82
Owner <noran@danon.tzafonet.org.il>
2020-02-23 09:30:25 +0200

    medicineAdapter

---
+++ app/src/main/java/com/example/ayah2019/AllMedicineFragment.java
@@ -0,0 +1,82 @@
+package com.example.ayah2019;
+
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import Ayah2019.MedicineAdapter;
+import Ayah2019.MyMedicine;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class AllMedicineFragment extends Fragment {
+    private MedicineAdapter medicineAdapter;
+    private ListView lvmedicine;
+
+
+    public AllMedicineFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        medicineAdapter = new MedicineAdapter(getContext());
+        View view = inflater.inflate(R.layout.fragment_all_medicine, container, false);
+        lvmedicine = view.findViewById(R.id.lsvMedicine);
+        lvmedicine.setAdapter(medicineAdapter);
+        return view;
+
+        //return inflater.inflate(R.layout.fragment_all_medicine, container, false);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        readMedicineFromFirebase();
+    }
+
+    public void readMedicineFromFirebase() {
+        FirebaseDatabase database = FirebaseDatabase.getInstance();//to connect to database
+        FirebaseAuth auth = FirebaseAuth.getInstance();//to get current UID
+        String uid = auth.getUid();
+        DatabaseReference reference = database.getReference();
+
+        reference.child("tasks").child(uid).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                medicineAdapter.clear();
+                for (DataSnapshot d : dataSnapshot.getChildren()) {
+                    MyMedicine m = d.getValue(MyMedicine.class);
+                    Log.d("MyMedicine", m.toString());
+                    medicineAdapter.add(m);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+
+        });
+    }
+}
--- app/src/main/java/com/example/ayah2019/All_medicine.java
+++ app/src/main/java/com/example/ayah2019/All_medicine.java
@@ -3,8 +3,13 @@ package com.example.ayah2019;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
+
+import Ayah2019.MedicineAdapter;
 
 public class All_medicine extends AppCompatActivity {
+  //  private MedicineAdapter medicineAdapter;
+    //private ListView lstmedicine;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
