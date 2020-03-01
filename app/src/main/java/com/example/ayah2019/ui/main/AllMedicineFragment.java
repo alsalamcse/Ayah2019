a643ac83e74a24b86c38a026f9f3e883c045fad2
Owner <noran@danon.tzafonet.org.il>
2020-03-01 12:15:06 +0200

    ...

--- app/src/main/java/com/example/ayah2019/ui/main/AllMedicineFragment.java
+++ app/src/main/java/com/example/ayah2019/ui/main/AllMedicineFragment.java
@@ -13,6 +13,7 @@ import android.view.ViewGroup;
 import android.widget.ListView;
 
 import com.example.ayah2019.R;
+
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
